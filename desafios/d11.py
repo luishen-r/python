95dacc8fe5c2a57e3488b9531ded7aa482848a39
Luis Henrique <luishenrique_s2129@hotmail.com>
2026-03-25 18:56:49 -0300

    Create d11.py

---
+++ desafios/d11.py
@@ -0,0 +1,5 @@
+lar=float(input('Qual a largura dessa parede? '))
+alt=float(input('Qual a altura dessa parede? '))
+area=lar*alt
+tinta=area/2
+print('A área dessa parede é de {}m², e a quantidade de tinta necessária para pintá-la é de {} litros.'.format(area,tinta))
