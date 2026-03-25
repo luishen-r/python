6bb1d3ed8a2bed8ef13bd012442b803217f73c7b
Luis Henrique <luishenrique_s2129@hotmail.com>
2026-03-25 10:02:52 -0300

    mudança na formatação

--- desafios/d2.py
+++ desafios/d2.py
@@ -1,4 +1,4 @@
 dia=input('Dia = ')
 mes=input('Mês = ')
 ano=input('Ano = ')
-print('Você nasceu no dia', dia, ', no mês', mes, 'e no ano', ano)
+print('Você nasceu no dia {}'.format(dia), ', no mês {}'.format(mes), 'e no ano {}' .format(ano))
