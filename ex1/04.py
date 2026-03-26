95e40a6dcf41d5992dc2f041ed2af51dbb2d7606
Luis Henrique <luishenrique_s2129@hotmail.com>
2026-03-26 15:40:05 -0300

    modulos

---
+++ ex1/04.py
@@ -0,0 +1,9 @@
+import math
+import random
+import emoji
+n=int(input('Digite um número: '))
+r=math.sqrt(n)
+print(f'A raiz quadrada de {n} é {math.ceil(r):.2f}')
+print(random.randint(1,100))
+print(emoji.emojize('Python é :polegar_para_cima:', language='pt'))
+
