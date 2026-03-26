8613b643788d8989a0cac08564e25cb80b0d0778
Luis Henrique <luishenrique_s2129@hotmail.com>
2026-03-25 21:09:22 -0300

    d15

---
+++ desafios/d15.py
@@ -0,0 +1,4 @@
+km=float(input("Diga a distância em quilômetros percorrida "))
+dia=int(input('Quantos dias de locação? '))
+valor=(60*dia)+(0.15*km)
+print(f"O valor a ser pago é de R${valor:.2f}")
