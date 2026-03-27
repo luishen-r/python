0dd45c06f75b46a733679df56bf76dc6ddfc1e47
Luis Henrique <luishenrique_s2129@hotmail.com>
2026-03-27 14:58:28 -0300

    desafios de string

---
+++ desafios/d25.py
@@ -0,0 +1,2 @@
+nome=str(input("Digite seu nome: ")).lower().strip()
+print(f'Seu nome tem Silva? {"silva" in nome}')
---
+++ desafios/d26.py
@@ -0,0 +1,4 @@
+fr=str(input('Digite uma frase: ')).strip().upper()
+print(f'A letra A aparece {fr.count("A")} vezes')
+print(f'A primeira letra A aparece na posição {fr.find("A")+1}')
+print(f'A última letra A aparece na posição {fr.rfind("A")+1-fr.count(" ")}')
