571d0ded0ab29e47f12cf9f39ef9382f36013645
Luis Henrique <luishenrique_s2129@hotmail.com>
2026-03-29 20:34:50 -0300

    Create main07.py

---
+++ ex1/main07.py
@@ -0,0 +1,7 @@
+name=str(input('Qual seu nome? '))
+if name == 'Luis':
+    print('Nome de goat')
+elif name in 'Pedro, João, Maria, Ana':
+    print('Seu nome é bem comum no nosso país')
+else:
+    print(f'Tenha um bom dia {name}')
