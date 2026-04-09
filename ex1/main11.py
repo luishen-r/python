f054aa2513ec8fe32e8a26345b973578b2823cff
Luis Henrique <luishenrique_s2129@hotmail.com>
2026-04-09 19:22:00 -0300

    tuplas

--- ex1/main11.py
+++ ex1/main11.py
@@ -5,4 +5,8 @@ for pos, c in enumerate(range(0, len(nomes))):
 print(sorted(nomes))
 pessoa = ('Luis', 18, 'M')
 print(pessoa[0:])
-    
+filme = ('O Poderoso Chefão', 1972, 'Francis Ford Coppola')
+titulo = filme[0]
+ano = filme[1]
+diretor = filme[2]
+print(f'O filme {titulo} foi lançado em {ano} e dirigido por {diretor}')
