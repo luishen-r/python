f2020a3e1460d7ec6879983d612b70237d77a392
Luis Henrique <luishenrique_s2129@hotmail.com>
2026-04-19 17:10:20 -0300

    feat: add

--- ex1/main12.py
+++ ex1/main12.py
@@ -8,8 +8,19 @@ for k, v in pessoas.items():
     
 estado1 = {'UF': 'Ceará', 'Capital': 'Fortaleza', 'População(em milhões)': 2.57}
 estado2 = {'UF': 'Piauí', 'Capital': 'Teresina', 'População(em milhões)': 3.28}
+
 nordeste = list()
 nordeste.extend([estado1, estado2])
+
+n = int(input('Quantos estados deseja cadastrar? '))
+for i in range(n):
+    estado = dict()
+    estado['UF'] = str(input('UF: '))
+    estado['Capital'] = str(input('Capital: '))
+    estado['População(em milhões)'] = float(input('População(em milhões): '))
+    nordeste.append(estado.copy())
+    print()
+
 for e in nordeste:
     for k, v in e.items():
         print(f'{k} = {v}')
