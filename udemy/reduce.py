0f3042f0676d936af027231eb6a65532cc5e0800
Luis Henrique <luishenrique_s2129@hotmail.com>
2026-06-11 20:21:01 -0300

    #

---
+++ udemy/reduce.py
@@ -0,0 +1,23 @@
+from functools import reduce
+
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+]
+
+
+def func_reduce(prev, prod):
+    print(prev)
+    return prev + prod['preco']
+
+
+total = reduce(
+    func_reduce,
+    produtos,
+    0
+)
+
+print(total)
